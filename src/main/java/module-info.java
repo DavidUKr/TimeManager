9a1f2ee8d86e3635d2134ad19e6353fab95d7e6d
DavidUKr <DavidUKr@users.noreply.github.com>
2023-04-19 11:35:28 +0300

    main

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module main.timemanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens main.timemanager to javafx.fxml;
-    exports main.timemanager;
-}

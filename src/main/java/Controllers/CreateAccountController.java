5f659dcac7f6dcd7a6172497c7783f13e0bb5bcf
DavidUKr <DavidUKr@users.noreply.github.com>
2023-04-20 13:59:42 +0300

    pages

---
+++ src/main/java/Controllers/CreateAccountController.java
@@ -0,0 +1,37 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+
+public class CreateAccountController {
+
+    @FXML
+    Button btnCreate;
+    @FXML
+    ToggleButton btnTogglePers_Buss;
+    @FXML
+    TextField txtFieldUsername;
+    @FXML
+    TextField txtFieldPassword;
+    @FXML
+    TextField txtFieldVerify;
+    @FXML
+    Label lblStatus;
+
+    public void switchPers_Buss(){
+        if(lblStatus.getText().equals("Personal")) lblStatus.setText("Business");
+        else lblStatus.setText("Personal");
+    }
+
+    public void createAcc(){
+
+        if(txtFieldPassword.getText().equals(txtFieldVerify.getText()))
+        System.out.println(txtFieldUsername.getText()+txtFieldPassword.getText());
+        else lblStatus.setText("Not same");
+    }
+
+}
---
+++ src/main/java/Controllers/LoginController.java
@@ -0,0 +1,54 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    Button btnLogin;
+    @FXML
+    TextField txtFieldUsername;
+    @FXML
+    TextField txtFieldPassword;
+    @FXML
+    Button btnCreateAccount;
+
+    Scene scene;
+    Stage stage;
+    Parent root;
+
+    public void login(ActionEvent event) throws IOException {
+        System.out.println("Username:"+txtFieldUsername.getText());
+        System.out.println("Pass:"+txtFieldPassword.getText());
+
+        changeToMain(event, "/UI/MainPage.fxml");
+    }
+
+    public void gotoCreateAcc(ActionEvent event) throws IOException {
+
+        changeToMain(event, "/UI/CreateAccount.fxml");
+    }
+
+    private void changeToMain(ActionEvent event, String path) throws IOException {
+
+        root= FXMLLoader.load(getClass().getResource(path));
+        scene=new Scene(root);
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}

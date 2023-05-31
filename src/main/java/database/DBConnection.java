9a6274c041a237a5b4e311d0327028ec63384946
DavidUKr <DavidUKr@users.noreply.github.com>
2023-05-31 15:30:47 +0300

    do stuff

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -12,7 +12,7 @@ public class DBConnection {
     private static Connection connection;
 
     public static Connection getConnection() {
-        String url="jdbc:postgresql://192.168.254.140:5432/TimeManager";
+        String url="jdbc:postgresql://192.168.133.162:5432/TimeManager";
         String user="timemanager";
         String password="tm1234";
 
